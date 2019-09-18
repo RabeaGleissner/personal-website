579b70c91460e8a60ecca1603a42c600b880da96
Rabea Gleissner <rabeagleissner@gmail.com>
2019-09-18 11:11:10 +0800

    Add favicon and update about me

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -6,9 +6,9 @@ export default() => {
   return (
     <Layout>
       <h1 className="about-me-title">About me</h1>
-      <p>Hello! I'm Rabea. I am a full-stack software engineer working fully remote from Singapore.</p>
+      <p>Hello! I'm Rabea. I am a full-stack software engineer based in Singapore.</p>
       <p>I currently work as a JavaScript developer, using React and Node. I also have experience with Ruby, TypeScript, Java and other languages.</p>
-      <p>After working in consulting for three years I now work for a British product company.</p>
+      <p>I spent three years working as a consultant at <a href="http://8thlight.com">8th Light</a> in London before I made the switch to a product company.</p>
       <p>This website is a bit of a <Link to="/blog-index/">collection of older and newer blog posts</Link> and <Link to="/til-index/">snippets of things that I learnt</Link>.</p>
     </Layout>
   )
